ca12abd174912277bf27c951eb0aac610122fd80
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2018-03-28 00:02:27 +0200

    Added new Glue Recipe using Rice Sludge

---
+++ 7.3.0/Mods/ECO Realism/RiceGlue.cs
@@ -0,0 +1,34 @@
+﻿namespace Eco.Mods.TechTree
+{
+    using System;
+    using Eco.Shared.Localization;
+    using System.Collections.Generic;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Skills;
+    using Eco.Shared.Utils;
+    using Eco.World;
+    using Eco.World.Blocks;
+    using Gameplay.Systems.TextLinks;
+
+    [RequiresSkill(typeof(CampfireCookingSkill), 3)]
+    public class RiceGlueRecipe : Recipe
+    {
+        public RiceGlueRecipe()
+        {
+            this.Products = new CraftingElement[]
+            {
+               new CraftingElement<GlueItem>(1),
+            };
+            this.Ingredients = new CraftingElement[]
+            {
+                new CraftingElement<RiceSludgeItem>(typeof(CampfireCookingEfficiencySkill), 2, CampfireCookingEfficiencySkill.MultiplicativeStrategy),
+                new CraftingElement<GlassJarItem>(1),
+            };
+            this.Initialize("Rice Glue", typeof(RiceGlueRecipe));
+            this.CraftMinutes = CreateCraftTimeValue(typeof(RiceGlueRecipe), this.UILink(), 3, typeof(CampfireCookingSpeedSkill));
+            CraftingComponent.AddRecipe(typeof(CampfireObject), this);
+        }
+    }
+}
