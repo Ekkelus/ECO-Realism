7ba410382faaaae5071c4cca83fea2b0376ccb36
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2018-04-02 13:56:56 +0200

    added "SuperSkills"
    1 (configurable) Efficiency skill can be leveled up to level 10, the name "SuperSkill" was a temporary solution for me, we can keep i, or change it if you want

--- 7.3.0/Mods/Autogen/Tech/BasicSmeltingEfficiency.cs
+++ 7.3.0/Mods/Autogen/Tech/BasicSmeltingEfficiency.cs
@@ -17,6 +17,7 @@ namespace Eco.Mods.TechTree
     using Eco.Shared.Services;
     using Eco.Shared.Utils;
     using Gameplay.Systems.Tooltip;
+	using EcoRealism.Utils;
 
     [Serialized]
     [RequiresSkill(typeof(BasicSmeltingSkill), 1)]    
@@ -26,13 +27,31 @@ namespace Eco.Mods.TechTree
         public override string Description { get { return Localizer.Do(""); } }
 
         public static ModificationStrategy MultiplicativeStrategy = 
-            new MultiplicativeStrategy(new float[] { 1, 1 - 0.1f, 1 - 0.2f, 1 - 0.3f, 1 - 0.4f, 1 - 0.5f });
+            new MultiplicativeStrategy(new float[] { 1, 1 - 0.1f, 1 - 0.2f, 1 - 0.3f, 1 - 0.4f, 1 - 0.5f, 1 - 0.55f, 1 - 0.6f, 1 - 0.65f, 1 - 0.7f, 1 - 0.75f });
         public static ModificationStrategy AdditiveStrategy =
             new AdditiveStrategy(new float[] { 0, 0.2f, 0.35f, 0.5f, 0.65f, 0.8f });
-        public static int[] SkillPointCost = { 5, 10, 15, 20, 25 };
+        public static int[] SkillPointCost = { 5, 10, 15, 20, 25, 30, 40, 50, 60, 70 };
         public override int RequiredPoint { get { return this.Level < this.MaxLevel ? SkillPointCost[this.Level] : 0; } }
         public override int PrevRequiredPoint { get { return this.Level - 1 >= 0 && this.Level - 1 < this.MaxLevel ? SkillPointCost[this.Level - 1] : 0; } }
-        public override int MaxLevel { get { return 5; } }
+        public override int MaxLevel { get { return 10; } }
+
+        public override IAtomicAction CreateLevelUpAction(Player player)
+        {
+            if (this.Level != 5) return base.CreateLevelUpAction(player);
+            if (SkillUtils.SuperSkillCount(player.User) >= ConfigHandler.maxsuperskills) return new FailedAtomicAction(Localizer.Do("You already have enough SuperSkills " + SkillUtils.SuperSkillCount(player.User) + "/" + ConfigHandler.maxsuperskills));
+            foreach (string id in SkillUtils.superskillconfirmed)
+            {
+                if (id == player.User.ID)
+                {
+                    SkillUtils.superskillconfirmed.Remove(id);
+                    return base.CreateLevelUpAction(player);
+                }
+            }
+
+            SkillUtils.ShowSuperSkillInfo(player);
+            return new FailedAtomicAction(Localizer.Do("You need to confirm first"));
+
+        }
     }
 
 }
