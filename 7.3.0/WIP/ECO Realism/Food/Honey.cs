a53e476612ee4be61469be26e2c16e1a75101fda
Ekkelus <36330679+Ekkelus@users.noreply.github.com>
2018-04-09 19:16:09 +0200

    WIP
    Pushed a new folder + some files I'm working on. Not finished.

---
+++ 7.3.0/WIP/ECO Realism/Food/Honey.cs
@@ -0,0 +1,50 @@
+namespace Eco.Mods.TechTree
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Mods.TechTree;
+    using Eco.Shared.Items;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+
+    [Serialized]
+    [Weight(200)]
+    public partial class HoneyItem :
+        FoodItem
+    {
+        public override string FriendlyName { get { return "Honey"; } }
+        public override string Description { get { return "A spoonful a day keeps the doctor away"; } }
+
+        private static Nutrients nutrition = new Nutrients() { Carbs = 1, Fat = 1, Protein = 1, Vitamins = 1 };
+        public override float Calories { get { return 50; } }
+        public override Nutrients Nutrition { get { return nutrition; } }
+    }
+
+    [RequiresSkill(typeof(BasicBakingSkill), 4)]
+    public partial class HoneyRecipe : Recipe
+    {
+        public HoneyRecipe()
+        {
+            this.Products = new CraftingElement[]
+            {
+                new CraftingElement<HoneyItem>(),
+                new CraftingElement<BeeswaxItem>(),
+
+            };
+            this.Ingredients = new CraftingElement[]
+            {
+                new CraftingElement<GlassJarItem>(typeof(BasicBakingEfficiencySkill), 2, BasicBakingEfficiencySkill.MultiplicativeStrategy),
+            };
+            this.CraftMinutes = CreateCraftTimeValue(typeof(HoneyRecipe), Item.Get<HoneyItem>().UILink(), 10, typeof(BasicBakingSpeedSkill));
+            this.Initialize("Honey", typeof(HoneyRecipe));
+            CraftingComponent.AddRecipe(typeof(BeehiveObject), this);
+        }
+    }
+}
