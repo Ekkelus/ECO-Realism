1e5bc1854d643a1d78f59063d14280e22791f818
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2018-04-28 02:58:43 +0200

    RottenFood (dummy for foodallergy)
    with the help of this item, the eaten food will be replaced by food with no nutrients in you stomach

---
+++ 7.4.X WIP/Mods/REYmod/Food/RottenFood.cs
@@ -0,0 +1,33 @@
+﻿namespace Eco.Mods.TechTree
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Mods.TechTree;
+    using Eco.Shared.Items;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.Shared.View;
+    
+    
+    /// <summary>
+    /// Dummy for the Foodallergysystem
+    /// </summary>
+    [Serialized]
+    [Weight(200)]
+    public partial class RottenFoodItem :
+        FoodItem
+    {
+        public override string FriendlyName { get { return "Rotten Food"; } }
+        public override string Description { get { return "Something you were allergic to"; } }
+
+        private static Nutrients nutrition = new Nutrients() { Carbs = 0, Fat = 0, Protein = 0, Vitamins = 0 };
+        public override float Calories { get { return 100; } }
+        public override Nutrients Nutrition { get { return nutrition; } }
+    }
+}
