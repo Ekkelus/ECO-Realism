b15e50ba24813ace5af3f11cb14292fde76a89a6
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2018-11-21 18:58:00 +0100

    Added Rebar + some other stuff
    typo in rules, fix lasers, outdated notification on changelog

--- 7.7.X/Mods/Objects/LaserObject.cs
+++ 7.7.X/Mods/Objects/LaserObject.cs
@@ -4,6 +4,7 @@
 using System;
 using System.Collections.Generic;
 using Eco.Gameplay.Components;
+using Eco.Gameplay.Components.Auth;
 using Eco.Gameplay.Objects;
 using Eco.Mods.TechTree;
 using Eco.Shared.Math;
@@ -17,6 +18,7 @@ using REYmod.Utils;
 [RequireComponent(typeof(ChargingComponent))]
 [RequireComponent(typeof(PowerGridNetworkComponent))]
 [RequireComponent(typeof(SpecificGroundComponent))]
+[RequireComponent(typeof(PropertyAuthComponent))]
 public class LaserObject : WorldObject
 {
     public float MinimapYaw { get { return 0.0f; } }
@@ -35,6 +37,5 @@ public class LaserObject : WorldObject
 
     static LaserObject()
     {
-        AddOccupancyList(typeof(LaserObject), new BlockOccupancy(Vector3i.Zero, typeof(WorldObjectBlock)));
     }
 }
