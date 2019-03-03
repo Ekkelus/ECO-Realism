ba9be39305ab9a01f54ebc64708e4223baa92650
AndyClausen <andreas.clausen@hotmail.com>
2019-03-03 19:36:02 +0100

    chore(tech): update basic engineering to 8.0 and remove hewing skill requirement

--- 7.7.X/Mods/Autogen/Tech/BasicEngineering.cs
+++ 7.7.X/Mods/Autogen/Tech/BasicEngineering.cs
@@ -1,23 +1,95 @@
 namespace Eco.Mods.TechTree
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Eco.Core.Utils;
+    using Eco.Core.Utils.AtomicAction;
     using Eco.Gameplay.Components;
     using Eco.Gameplay.DynamicValues;
     using Eco.Gameplay.Items;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
     using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems.TextLinks;
     using Eco.Shared.Localization;
     using Eco.Shared.Serialization;
+    using Eco.Shared.Services;
+    using Eco.Shared.Utils;
+    using Gameplay.Systems.Tooltip;
 
     [Serialized]
-    [RequiresSkill(typeof(EngineerSkill), 0)]    
+    [RequiresSkill(typeof(EngineerSkill), 0)]
     public partial class BasicEngineeringSkill : Skill
     {
         public override LocString DisplayName { get { return Localizer.DoStr("Basic Engineering"); } }
-        public override LocString DisplayDescription { get { return Localizer.DoStr(""); } }
+        public override LocString DisplayDescription { get { return Localizer.DoStr("Basic Engineering allows for the easier construction of roads and early forms of mechanical power. Level by crafting related recipes."); } }
 
-        public static int[] SkillPointCost = { 1, 1, 1, 1, 1 };
-        public override int RequiredPoint { get { return this.Level < this.MaxLevel ? SkillPointCost[this.Level] : 0; } }
-        public override int PrevRequiredPoint { get { return this.Level - 1 >= 0 && this.Level - 1 < this.MaxLevel ? SkillPointCost[this.Level - 1] : 0; } }
-        public override int MaxLevel { get { return 1; } }
+        public override void OnLevelUp(User user)
+        {
+            user.Skillset.AddExperience(typeof(SelfImprovementSkill), 20, Localizer.DoStr("for leveling up another specialization."));
+        }
+
+
+        public static ModificationStrategy MultiplicativeStrategy =
+            new MultiplicativeStrategy(new float[] { 1,
+
+                1 - 0.5f,
+
+                1 - 0.55f,
+
+                1 - 0.6f,
+
+                1 - 0.65f,
+
+                1 - 0.7f,
+
+                1 - 0.75f,
+
+                1 - 0.8f,
+
+            });
+        public override ModificationStrategy MultiStrategy { get { return MultiplicativeStrategy; } }
+        public static ModificationStrategy AdditiveStrategy =
+            new AdditiveStrategy(new float[] { 0,
+
+                0.5f,
+
+                0.55f,
+
+                0.6f,
+
+                0.65f,
+
+                0.7f,
+
+                0.75f,
+
+                0.8f,
+
+            });
+        public override ModificationStrategy AddStrategy { get { return AdditiveStrategy; } }
+        public static int[] SkillPointCost = {
+
+            1,
+
+            1,
+
+            1,
+
+            1,
+
+            1,
+
+            1,
+
+            1,
+
+        };
+        public override int RequiredPoint { get { return this.Level < SkillPointCost.Length ? SkillPointCost[this.Level] : 0; } }
+        public override int PrevRequiredPoint { get { return this.Level - 1 >= 0 && this.Level - 1 < SkillPointCost.Length ? SkillPointCost[this.Level - 1] : 0; } }
+        public override int MaxLevel { get { return 7; } }
+        public override int Tier { get { return 2; } }
     }
 
     [Serialized]
@@ -42,7 +114,7 @@ namespace Eco.Mods.TechTree
             };
             this.Ingredients = new CraftingElement[]
             {
-                new CraftingElement<HewnLogItem>(typeof(ResearchEfficiencySkill), 10, ResearchEfficiencySkill.MultiplicativeStrategy), 
+                new CraftingElement<HewnLogItem>(10)
             };
             this.CraftMinutes = new ConstantValue(5);
 
