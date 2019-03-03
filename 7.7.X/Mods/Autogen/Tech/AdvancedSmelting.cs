cbb06661553bd9b5e41c6e485c3323996c0299e0
AndyClausen <andreas.clausen@hotmail.com>
2019-03-03 18:08:08 +0100

    chore(tech): update advanced smelting to 8.0 and add book ingredient

--- 7.7.X/Mods/Autogen/Tech/AdvancedSmelting.cs
+++ 7.7.X/Mods/Autogen/Tech/AdvancedSmelting.cs
@@ -1,23 +1,95 @@
 namespace Eco.Mods.TechTree
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Eco.Core.Utils;
+    using Eco.Core.Utils.AtomicAction;
     using Eco.Gameplay.Components;
     using Eco.Gameplay.DynamicValues;
     using Eco.Gameplay.Items;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Property;
     using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems.TextLinks;
     using Eco.Shared.Localization;
     using Eco.Shared.Serialization;
+    using Eco.Shared.Services;
+    using Eco.Shared.Utils;
+    using Gameplay.Systems.Tooltip;
 
     [Serialized]
     [RequiresSkill(typeof(SmithSkill), 0)]    
     public partial class AdvancedSmeltingSkill : Skill
     {
-        public override LocString DisplayName { get { return Localizer.DoStr("Advanced Smelting"); } }
-        public override LocString DisplayDescription { get { return Localizer.DoStr(""); } }
+        public override LocString DisplayName        { get { return Localizer.DoStr("Advanced Smelting"); } }
+        public override LocString DisplayDescription { get { return Localizer.DoStr("Advanced smelting aids in the production of steel - a key ingredient in the progress of any group. Level by crafting advanced smelting recipes."); } }
 
-        public static int[] SkillPointCost = { 1, 1, 1, 1, 1 };
-        public override int RequiredPoint { get { return this.Level < this.MaxLevel ? SkillPointCost[this.Level] : 0; } }
-        public override int PrevRequiredPoint { get { return this.Level - 1 >= 0 && this.Level - 1 < this.MaxLevel ? SkillPointCost[this.Level - 1] : 0; } }
-        public override int MaxLevel { get { return 1; } }
+        public override void OnLevelUp(User user)
+        {
+            user.Skillset.AddExperience(typeof(SelfImprovementSkill), 20, Localizer.DoStr("for leveling up another specialization."));
+        }
+
+
+        public static ModificationStrategy MultiplicativeStrategy = 
+            new MultiplicativeStrategy(new float[] { 1,
+                
+                1 - 0.5f,
+                
+                1 - 0.55f,
+                
+                1 - 0.6f,
+                
+                1 - 0.65f,
+                
+                1 - 0.7f,
+                
+                1 - 0.75f,
+                
+                1 - 0.8f,
+                
+            });
+        public override ModificationStrategy MultiStrategy { get { return MultiplicativeStrategy; } }
+        public static ModificationStrategy AdditiveStrategy =
+            new AdditiveStrategy(new float[] { 0,
+                
+                0.5f,
+                
+                0.55f,
+                
+                0.6f,
+                
+                0.65f,
+                
+                0.7f,
+                
+                0.75f,
+                
+                0.8f,
+                
+            });
+        public override ModificationStrategy AddStrategy { get { return AdditiveStrategy; } }
+        public static int[] SkillPointCost = {
+            
+            1,
+            
+            1,
+            
+            1,
+            
+            1,
+            
+            1,
+            
+            1,
+            
+            1,
+            
+        };
+        public override int RequiredPoint { get { return this.Level < SkillPointCost.Length ? SkillPointCost[this.Level] : 0; } }
+        public override int PrevRequiredPoint { get { return this.Level - 1 >= 0 && this.Level - 1 < SkillPointCost.Length ? SkillPointCost[this.Level - 1] : 0; } }
+        public override int MaxLevel { get { return 7; } }
+        public override int Tier { get { return 4; } }
     }
 
     [Serialized]
@@ -43,10 +115,10 @@ namespace Eco.Mods.TechTree
             };
             this.Ingredients = new CraftingElement[]
             {
-                new CraftingElement<CoalItem>(typeof(ResearchEfficiencySkill), 200, ResearchEfficiencySkill.MultiplicativeStrategy),
-                new CraftingElement<IronIngotItem>(typeof(ResearchEfficiencySkill), 100, ResearchEfficiencySkill.MultiplicativeStrategy),
-                new CraftingElement<BrickItem>(typeof(ResearchEfficiencySkill), 100, ResearchEfficiencySkill.MultiplicativeStrategy),
-                new CraftingElement<BookItem>(typeof(ResearchEfficiencySkill), 4, ResearchEfficiencySkill.MultiplicativeStrategy),
+                new CraftingElement<CoalItem>(200),
+                new CraftingElement<IronIngotItem>(100),
+                new CraftingElement<BrickItem>(100),
+                new CraftingElement<BookItem>(4)
             };
             this.CraftMinutes = new ConstantValue(30);
 
