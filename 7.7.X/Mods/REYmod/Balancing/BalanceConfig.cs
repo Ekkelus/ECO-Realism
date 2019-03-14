188a7aaa3951afd6d878077ee2ca6e6a3aea07d0
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2019-03-14 16:25:21 +0100

    feat(config): add multipliers for seed drop and yield with example implementation in beans (#27)

---
+++ 7.7.X/Mods/REYmod/Balancing/BalanceConfig.cs
@@ -0,0 +1,52 @@
+﻿using Eco.Core.Controller;
+using Eco.Shared.Localization;
+
+namespace REYmod.Config
+{
+    public partial class REYconfig
+    {
+        #region Organisms
+
+        #region Animals
+
+        #endregion
+
+        #region Plants
+
+
+
+        private float _plantYieldMultiplier = .5f;
+        private float _seedDropMultiplier;
+
+        [LocDescription(
+            "How much should plant yield be multiplied with (where 0.5 is 50%, 1 is 100%, etc.)? Default: 0.5")]
+        [SyncToView]
+        public float PlantYieldMultiplier
+        {
+            get { return _plantYieldMultiplier; }
+            set
+            {
+                this.Changed("PlantYieldMultiplier");
+                _plantYieldMultiplier = value;
+            }
+        }
+
+        public float SeedDropMultiplier
+        {
+            get { return _seedDropMultiplier; }
+            set
+            {
+                this.Changed("SeedDropMultiplier");
+                _seedDropMultiplier = value;
+            }
+        }
+
+        #endregion
+
+        #region Trees
+
+        #endregion
+
+        #endregion
+    }
+}
