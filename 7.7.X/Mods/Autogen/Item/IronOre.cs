2cd440bb56bc167329f979d2342982fbd7b3148c
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2019-04-02 00:22:34 +0200

    restored ores to default
    the missing IRepresentsItem Interface was causing disconnects, i also reverted the other changes made to ores

--- 7.7.X/Mods/Autogen/Item/IronOre.cs
+++ 7.7.X/Mods/Autogen/Item/IronOre.cs
@@ -1,35 +1,49 @@
 namespace Eco.Mods.TechTree
 {
     using System;
-    using Gameplay.Items;
-    using Gameplay.Objects;
-    using Shared.Localization;
-    using Shared.Serialization;
-    using World;
-    using World.Blocks;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.World;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Pipes;
 
 
     [Serialized]
-    [Minable(0), Solid,Wall]
+    [Minable(1), Solid,Wall]
     public partial class IronOreBlock :
-        Block           
-    { }
+        Block            
+        , IRepresentsItem     
+    {
+        public Type RepresentedItemType { get { return typeof(IronOreItem); } }    
+    }
 
     [Serialized]
     [MaxStackSize(20)]                           
-    [Weight(15000)]      
+    [Weight(30000)]      
     [ResourcePile]                                          
     [Currency]              
     public partial class IronOreItem :
     BlockItem<IronOreBlock>
     {
-        public override LocString DisplayName { get { return Localizer.DoStr("Iron Ore"); } }
+        public override LocString DisplayName { get { return Localizer.DoStr("Iron Ore"); } } 
         public override LocString DisplayNamePlural { get { return Localizer.DoStr("Iron Ore"); } } 
         public override LocString DisplayDescription { get { return Localizer.DoStr("Unrefined ore with traces of iron."); } }
 
         public override bool CanStickToWalls { get { return false; } }  
 
-        private static Type[] blockTypes = new[] {
+        private static Type[] blockTypes = new Type[] {
             typeof(IronOreStacked1Block),
             typeof(IronOreStacked2Block),
             typeof(IronOreStacked3Block),
