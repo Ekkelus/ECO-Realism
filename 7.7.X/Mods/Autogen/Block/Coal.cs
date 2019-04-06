c06df4703563c527b1749fe0b423d82c53e266b2
Yamor123 <37719473+Yamor123@users.noreply.github.com>
2019-04-06 15:18:31 +0200

    moved blocks from items to blocks

--- 7.7.X/Mods/Autogen/Block/Coal.cs
+++ 7.7.X/Mods/Autogen/Block/Coal.cs
@@ -1,36 +1,50 @@
 namespace Eco.Mods.TechTree
 {
     using System;
-    using Gameplay.Items;
-    using Gameplay.Objects;
-    using Shared.Localization;
-    using Shared.Serialization;
-    using World;
-    using World.Blocks;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using Eco.Gameplay.Blocks;
+    using Eco.Gameplay.Components;
+    using Eco.Gameplay.DynamicValues;
+    using Eco.Gameplay.Items;
+    using Eco.Gameplay.Objects;
+    using Eco.Gameplay.Players;
+    using Eco.Gameplay.Skills;
+    using Eco.Gameplay.Systems;
+    using Eco.Gameplay.Systems.TextLinks;
+    using Eco.Shared.Localization;
+    using Eco.Shared.Serialization;
+    using Eco.Shared.Utils;
+    using Eco.World;
+    using Eco.World.Blocks;
+    using Eco.Gameplay.Pipes;
 
 
     [Serialized]
-    [Minable(0), Solid,Wall]
+    [Minable(1), Solid,Wall]
     public partial class CoalBlock :
-        Block           
-    { }
+        Block            
+        , IRepresentsItem     
+    {
+        public Type RepresentedItemType { get { return typeof(CoalItem); } }    
+    }
 
     [Serialized]
     [MaxStackSize(20)]                           
-    [Weight(15000)]      
-    [Fuel(20000)]          
+    [Weight(30000)]      
+    [Fuel(20000)][Tag("Fuel")]          
     [ResourcePile]                                          
     [Currency]              
     public partial class CoalItem :
     BlockItem<CoalBlock>
     {
-        public override LocString DisplayName { get { return Localizer.DoStr("Coal"); } }
+        public override LocString DisplayName { get { return Localizer.DoStr("Coal"); } } 
         public override LocString DisplayNamePlural { get { return Localizer.DoStr("Coal"); } } 
         public override LocString DisplayDescription { get { return Localizer.DoStr("A combustible mineral which when used as a fuel provides lots of energy but generates lots of pollution."); } }
 
         public override bool CanStickToWalls { get { return false; } }  
 
-        private static Type[] blockTypes = new[] {
+        private static Type[] blockTypes = new Type[] {
             typeof(CoalStacked1Block),
             typeof(CoalStacked2Block),
             typeof(CoalStacked3Block),
